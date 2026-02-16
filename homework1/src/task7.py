6754fadd4085f3b277aee12e7bf16bbfc0a631ca
Ranis <ranis.hamidullin36@gmail.com>
2026-02-16 10:38:08 +0000

    task 7 almost comp

---
+++ homework1/src/task7.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+matrix1 = np.array([[1,2,3],
+                   [3,2,1],
+                   [4,2,2]])
+
+matrix2 = np.array([[7,2,1],
+                   [5,9,8],
+                   [1,7,5]])
+
+def addMatrices(matrix1, matrix2):
+    return np.add(matrix1, matrix2)
+
+def dotMatrices(matrix1, matrix2):
+    return np.dot(matrix1, matrix2)
+
+def crossMatrices(matrix1, matrix2):
+    return np.cross(matrix1, matrix2)
+
+
+print(addMatrices(matrix1, matrix2))
+print(dotMatrices(matrix1, matrix2))
+print(crossMatrices(matrix1, matrix2))
---
+++ homework1/tests/test_task7.py
@@ -0,0 +1,12 @@
+from src.task7 import matrix1, matrix2, addMatrices, dotMatrices, crossMatrices
+import numpy as np
+
+def test_addMatrices():
+
+
+def test_dotMatrices():
+
+
+def test_crossMatrices():
+
+
