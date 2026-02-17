0c554220baa2f1829af7faef0ddaaf2392f4bd36
Ranis <ranis.hamidullin36@gmail.com>
2026-02-17 02:46:17 +0000

    task7 comp

--- homework1/tests/test_task7.py
+++ homework1/tests/test_task7.py
@@ -2,11 +2,30 @@ from src.task7 import matrix1, matrix2, addMatrices, dotMatrices, crossMatrices
 import numpy as np
 
 def test_addMatrices():
-
+    result = addMatrices(matrix1, matrix2)
+    assert isinstance(result, np.ndarray)
+    assert result.shape == (3, 3)
+    expectedAns = np.array([[ 8,  4,  4],
+                              [ 8, 11,  9],
+                              [ 5,  9,  7]])
+    np.testing.assert_array_equal(result, expectedAns)
 
 def test_dotMatrices():
-
+    result = dotMatrices(matrix1, matrix2)
+    assert isinstance(result, np.ndarray)
+    assert result.shape == (3, 3)
+    expectedAns = np.array([[ 20,  41,  32],
+                              [ 32, 31,  24],
+                              [ 40,  40,  30]])
+    np.testing.assert_array_equal(result, expectedAns)
 
 def test_crossMatrices():
+    result = crossMatrices(matrix1, matrix2)
+    assert isinstance(result, np.ndarray)
+    assert result.shape == (3, 3)
 
+    expectedAns = np.array([[ -4,  20,  -12],
+                              [ 7, -19,  17],
+                              [ -4,  -18,  26]])
+    np.testing.assert_array_equal(result, expectedAns)
 
